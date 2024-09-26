474ea2ab60e6bbf6c58b5842385672469df9955e
romany10203 <romanym2020@gmail.com>
2024-09-26 22:52:15 +0300

    creating initiator for the bot and updating the templates

--- src/app/api/whatsapp/route.js
+++ src/app/api/whatsapp/route.js
@@ -7,8 +7,9 @@ const client = new Twilio(accountSid, authToken);
 let userSessions = {};
 const twilioWhatsAppNumber = 'whatsapp:+18633445007';
 const supNumber = 'whatsapp:+573197741990';
-const hrNumber = '';
-const invliadMSGNum = 10
+const hrNumber = 'whatsapp:+593991434326';
+const initiator = 'whatsapp:+51945628224'
+const invalidMSGNum = 10
 
 function reminder() {  
   Object.keys(userSessions).forEach((number) => {
@@ -32,11 +33,12 @@ function setLastSendTime(number) {
   console.log('Updated user:', userSessions[number]);
 }
 
-function getUserSession(number) {
+function getUserSession(number,name) {
   let newUser = false;
   if (!userSessions[number]) {
     newUser = true;
     userSessions[number] = {
+      name:name,
       readEmail:false,
       logged:false,
       seeCalls:false,
@@ -44,13 +46,13 @@ function getUserSession(number) {
       secondScreenId: null,
       thirdScreenId: null,
       waitingImage:false,
-      invlaidMessages:0,
+      invalidMessages:0,
       waiting:false,
       done:false,
       lastSendTime:Date.now()
     };
   }
-  return [userSessions[number],newUser];
+  return userSessions[number];
 }
 
 export async function POST(req) {
@@ -59,24 +61,38 @@ export async function POST(req) {
     const params = new URLSearchParams(body);
     console.log(params);
     
-    const name = params.get('ProfileName');
     const whatsappNumber = params.get('From');
     const waID = params.get('WaId');
     const text = params.get('Body')?.toLowerCase().trim();
     const buttonId = params.get('ButtonPayload');
-    const buttonText = params.get('ButtonText');
     
     if (whatsappNumber === supNumber) {
       const [number, solver] = buttonId.split(/_(.+)/);
       console.log(number,solver);
       userSessions[number].waiting = false;
-      await problemSolved(number,solver)
-      await sendMessageReply(whatsappNumber,`Thank you, I notified the applicant.`)
+      await problemSolved(number,solver);
+      await sendMessageReply(whatsappNumber,`Thank you, I notified the applicant.`);
       return new Response('', { status: 200 });
     }
 
-    let [user, newUser] = getUserSession(whatsappNumber);
+    if (whatsappNumber === initiator) {
+      let [newNumber,newName] = text.split(',');
+      getUserSession(newNumber,newName);
+      await client.messages.create({
+        from:twilioWhatsAppNumber,
+        to:newNumber,
+        contentSid: 'HX03991f1e7525d4ca3949640bbabe05d3',
+        contentVariables: JSON.stringify({
+          name:newName,
+          yesOption:'read',
+          noOption:'read',
+        })
+      });
+      return new Response('', { status: 200 });
+    }
 
+    let user = getUserSession(whatsappNumber);
+    const name = user.name;
     // options must be [{id:'ID',title:''}]    
     // ID=> your id that coming in the request  title=> the text that showed in the user chat
 
@@ -84,7 +100,7 @@ export async function POST(req) {
     // applicant not following the instractions.
 
     if (user.waiting) {
-      await sendMessageReply(whatsappNumber,'Please, be patient and wait until the issue is solved')
+      await sendMessageReply(whatsappNumber,'Please, be patient and wait until the issue is solved');
       return new Response('', { status: 200 });
     }
     
@@ -94,6 +110,11 @@ export async function POST(req) {
     }
 
     if (user.done) {
+      return new Response('', { status: 200 });
+    }
+
+    if (user.invalidMessages >= invalidMSGNum){
+      await sendMessageReply(whatsappNumber,'Please, wait until one member of our IT support gets in contact with you.')
       return new Response('', { status: 200 });
     }
 
@@ -114,7 +135,7 @@ export async function POST(req) {
         await client.messages.create({
           from:twilioWhatsAppNumber,
           to:whatsappNumber,
-          contentSid: 'HXfb581d2e44c8a55ecf43387a3b1d61c9',
+          contentSid: 'HX12ad5380133792f4b489b2f3cbcd12a5',
           contentVariables: JSON.stringify({
             yesOption:'first_hung',
             noOption:'first_hung',
@@ -130,7 +151,7 @@ export async function POST(req) {
         await client.messages.create({
           from:twilioWhatsAppNumber,
           to:whatsappNumber,
-          contentSid: 'HX6bb587bb61f67980d3c4255d9e357622',
+          contentSid: 'HX0faa7e154d326ebb75a32a329c19766a',
           contentVariables: JSON.stringify({
             yesOption:'hung',
             noOption:'hung',
@@ -143,48 +164,37 @@ export async function POST(req) {
       return new Response('', { status: 200 });
     }
 
-    if (text === 'start') {
-      await client.messages.create({
-        from:twilioWhatsAppNumber,
-        to:whatsappNumber,
-        contentSid: 'HXd3a64fe0bda7ba905ea09f7cfe14ee65',
-        contentVariables: JSON.stringify({
-          name:name,
-          yesOption:'read',
-          noOption:'read',
-        }),
-      });
-    }
-    else if (buttonId === 'no_read') {
-      await sendYesNoOption(whatsappNumber,name,'HX1e0e2461298dd5d4180e40d4ada7f244','read the email','read');
+
+    if (buttonId === 'no_read') {
+      await sendYesNoOption(whatsappNumber,name,'HX43bc1c1f84ba338deddccef7b3bf1d56','read the email','read');
     }
     else if (buttonId === 'yes_read') {
-      await sendYesNoOptions(whatsappNumber,'HX962a2a42bfb1b318e13741083ea729bf','logged');
+      await sendYesNoOptions(whatsappNumber,'HX880467ac87fc57ff0efb635aa228df98','logged');
     }
     else if (buttonId === 'yes_logged') {
       userSessions[whatsappNumber].logged = true;
-      await sendYesNoOptions(whatsappNumber,'HXff58415e0a26cc8e8910c0d1e6d5f250','see_calls');
+      await sendYesNoOptions(whatsappNumber,'HX93b6e6a585101daa6421387f779ea27f','see_calls');
     }
     else if (buttonId === 'yes_see_calls') {
       userSessions[whatsappNumber].seeCalls = true;
       userSessions[whatsappNumber].waitingImage = true;
-      await sendMessageReply(whatsappNumber, 'Perfect, please, call the test call with number *14049203888*. This will ask you to enter your access code. For the purpose of this test, enter any random code like 1111111. After entering this, you will hear that the code is incorrect. Don\'t worry, that is expected to happen. That will mean that the call was successful and the dial pad is working. Please, take a screenshot of this and after it, proceed to hang up the call.\nUpload screenshot photo to continue.')
+      await sendMessageReply(whatsappNumber, 'Perfect, please, call the *test call* with number *14049203888*. This will ask you to enter your access code. For the purpose of this test, enter any random code like 1111111. After entering this, you will hear that the code is incorrect. Don\'t worry, that is expected to happen. That will mean that the call was successful and the dial pad is working. Please, take a screenshot of this and after it, proceed to hang up the call.\nUpload screenshot photo to continue.')
     }
     else if (buttonId === 'no_first_hung') {
-      await sendYesNoOption(whatsappNumber,name,'HX1e0e2461298dd5d4180e40d4ada7f244','hung up','first_hung');
+      await sendYesNoOption(whatsappNumber,name,'HX43bc1c1f84ba338deddccef7b3bf1d56','hang up','first_hung');
     }
     else if (buttonId === 'yes_first_hung') {
-      await sendYesNoOptions(whatsappNumber,'HXba5fb13b7adcac70aaef22f297084833','voice_clear');
+      await sendYesNoOptions(whatsappNumber,'HXde028ba788bf9c07966650b66007e0b6','voice_clear');
     }
     else if (buttonId === 'yes_voice_clear') {
       userSessions[whatsappNumber].waitingImage = true;
       await sendMessageReply(whatsappNumber,`Now, call the *test call* with number *14049203817*. This will connect you with the *ALTA* direct line. If you manage to hear the options provided by the automatic responder, take a screenshot of it, and hang up the call. Repeat this with the number *18884654648*.\nPlease send the 2 screenshots to continue.`)
     }
     else if (buttonId === 'no_hung') {
-      await sendYesNoOption(whatsappNumber,name,'HX1e0e2461298dd5d4180e40d4ada7f244','hang up','hung')
+      await sendYesNoOption(whatsappNumber,name,'HX43bc1c1f84ba338deddccef7b3bf1d56','hang up','hung')
     }
     else if (buttonId === 'yes_hung') {
-      await sendYesNoOptions(whatsappNumber,'HX60b49497f274ee35e97cd4dad027c286','voice_clear_finish');
+      await sendYesNoOptions(whatsappNumber,'HXe9185f2584e83ffd2cbb2f39a83cc5ae','voice_clear_finish');
     }
     else if (buttonId === 'yes_voice_clear_finish') {
       userSessions[whatsappNumber].done = true;
@@ -195,31 +205,38 @@ export async function POST(req) {
         -	In case the access code doesn't work, hang up the call immediately and call any of the Contingency Numbers *14049203817* or *18884654648*. In any of these lines, you must explain the issue that you have experienced, providing your identification and access code, and require them to proceed with the evaluation.
         \nThat’s it for now. Thanks for your time`)
 
+      await sendMessageReply(hrNumber,`The applicant ${name} ${waID} has finished the first check for the ALTA evaluation.`)
+
       //await sendMessageReply() to the support to notify him
     }
     else if (buttonId === 'no_logged') {
       userSessions[whatsappNumber].waiting = true;
       await sendMessageReply(whatsappNumber,'A technical assistant from our team will contact you. Please, be patient.')
-      await sendSupport(supNumber,'HX5116bc28d6879593741f57dd6aad69c0',name,waID,whatsappNumber);
+      await sendSupport(supNumber,'HX30afc9bbda97af3fede6a8088e23f076',name,waID,whatsappNumber);
     }
     else if (buttonId === 'no_see_calls') {
       userSessions[whatsappNumber].waiting = true;
       await sendMessageReply(whatsappNumber,'A technical assistant from our team will contact you. Please, be patient.')
-      await sendSupport(supNumber,'HXafde30b58c78afe57b59a1cc35f7f8d2',name,waID,whatsappNumber);
+      await sendSupport(supNumber,'HX71fa9f199d66c829ca0232516513b005',name,waID,whatsappNumber);
     }
     else if (buttonId === 'no_voice_clear') {
       userSessions[whatsappNumber].waiting = true;
       await sendMessageReply(whatsappNumber,'A technical assistant from our team will contact you. Please, be patient.')
-      await sendSupport(supNumber,'HXd73bfec78e24b0cbcc41ff94de772d3f',name,waID,whatsappNumber);
+      await sendSupport(supNumber,'HX03430b41d64f4332de3f29cbc54d3c6c',name,waID,whatsappNumber);
     }
     else if (buttonId === 'no_voice_clear_finish') {
       userSessions[whatsappNumber].waiting = true;
       await sendMessageReply(whatsappNumber,'A technical assistant from our team will contact you. Please, be patient.')
-      await sendSupport(supNumber,'HX23950f59981f081d6155fe3729163eaf',waID,whatsappNumber);
+      await sendSupport(supNumber,'HXf8e224bab3e2940b05020675ca233afa',name,waID,whatsappNumber);
     }
     else {
-      userSessions[whatsappNumber].invlaidMessages+=1;
-      await sendMessageReply(whatsappNumber,'Please, choose an option from the previous list');
+      userSessions[whatsappNumber].invalidMessages+=1;
+      if (userSessions[whatsappNumber].invalidMessages >= invalidMSGNum){
+        await invalidMessages(whatsappNumber,name,waID);
+      }
+      else {
+        await sendMessageReply(whatsappNumber,'Please, choose an option from the previous list');
+      }
     }
 
     return new Response('', { status: 200 });
@@ -254,12 +271,13 @@ const sendYesNoOption = async function (number,name,contentSID,step,option) {
   })
 }
 
-const sendSupport = async function (number,contentSID,applicantNumber,applicantWA) {
+const sendSupport = async function (number,contentSID,applicantName,applicantNumber,applicantWA) {
   await client.messages.create({
     from:twilioWhatsAppNumber,
     to:number,
     contentSid: contentSID,
     contentVariables: JSON.stringify({
+      name:applicantName,
       applicantNumber:applicantNumber,
       applicantWA:applicantWA,
     }),
@@ -283,10 +301,14 @@ const problemSolved = async function (number,solver) {
   await client.messages.create({
     from:twilioWhatsAppNumber,
     to:number,
-    contentSid: 'HXbeca98f80cc85b33fbf4c23144f78eca',
+    contentSid: 'HX73c114169d8bc2f36938a81ee4043f03',
     contentVariables: JSON.stringify({
       solver:solver,
     }),
   })
 }
 
+const invalidMessages = async function (number,name,waID) {
+  await sendMessageReply(number,'It seems you have doubts about how to proceed. I will refer you to our IT department so you can get assistance.');
+  await sendMessageReply(supNumber,`The applicant is not following the instructions.\n${name} ${waID}`);
+}
